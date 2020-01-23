854427f1518d3ee4035d872401c3f6e965b53d99
icekomo <ios@gdovindesigns.com>
2020-01-23 11:14:48 -0500

    burger click animtion

--- dev/js/burger/burger-click.js
+++ dev/js/burger/burger-click.js
@@ -1,3 +1,9 @@
 $("#burger-container").on("click", function(){
     console.log("click");
+    gsap.set("#outline",{alpha:1});
+    var lineToX = gsap.timeline();
+    lineToX.to("#burger-container",{duration:0.25, rotate:-180},"create-x")
+    .to("#top-line",{duration:0.25, rotate:45},"create-x")
+    .to("#bottom-line",{duration:0.25, rotate:-45},"create-x")
+    .from("#outline",{duration:0.5, drawSVG:"0"},"create-x");
 });
